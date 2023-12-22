31c1afba029375a437aa4cbf58552591bfa0af59
Musa <musakavus@gmail.com>
2023-12-23 01:34:52 +0300

    last touches

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 function myFunction() {
     var sidebar_overlay = document.getElementById("sidebar-overlay");
-    if (sidebar_overlay.style.right === "-500px") {
-        sidebar_overlay.style.right = "0";
+    var body_overlay = document.getElementById("body_overlay");
+
+    if (sidebar_overlay.style.right === "0") {
+        // sidebar'ı aç
+        sidebar_overlay.style.right = "-340px";
+        body_overlay.style.opacity = "1";
     } else {
-        sidebar_overlay.style.right = "-500px";
+        // sidebar'ı kapat
+        sidebar_overlay.style.right = "0";
+
+        body_overlay.style.opacity = "0.5";
     }
 }
 
 function close_sidebar() {
     var sidebar_overlay = document.getElementById("sidebar-overlay");
-    if (sidebar_overlay.style.right === "-500px") {
+    var body_overlay = document.getElementById("body_overlay");
+    if (sidebar_overlay.style.right === "0") {
         sidebar_overlay.style.right = "0";
+
     } else {
-        sidebar_overlay.style.right = "-500px";
+        sidebar_overlay.style.right = "-340px";
+        body_overlay.style.opacity = "1";
     }
 }
